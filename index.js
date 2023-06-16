7619e253eb16634dfc4a6c7e2a96c21cc4dfb2d4
Robert Popita <robertpopita@Roberts-Mac-mini.local>
2023-06-16 18:32:59 +0300

    add showProjects and showLanguages functions

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 function showHome() {
-    var page = document.getElementById("home");
-    page.style.display = "block";
+  var page = document.getElementById("home");
+  page.style.display = "block";
 }
 
 function showSkills() {
-    var page = document.getElementById("skills");
-    page.style.display = "block";    
+  var page = document.getElementById("skills");
+  page.style.display = "block";
+}
+
+function showLanguages() {
+  var page = document.getElementById("languages");
+  page.style.display = "block";
+}
+
+function showProjects() {
+  var page = document.getElementById("projects");
+  page.style.display = "block";
 }
 
 showHome();
