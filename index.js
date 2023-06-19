ab2e24101b743370601a16002482bc0cdd4b8d05
rpopita <robert.popita@linnify.com>
2023-06-19 20:03:54 +0300

    use activePage to store last visible page

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
+var activePage = "home";
+
+// utilities functions
+
 function hide(id) {
   console.info("hide %o element", id);
   document.getElementById(id).style.display = "none";
 }
 
-function hideAllPages() {
-  hide("home");
-  hide("skills");
-  hide("languages");
-  hide("projects");
-}
-
 function showPage(id) {
-  hideAllPages();
+  hide(activePage);
   var page = document.getElementById(id);
   console.info("show", page);
   page.style.display = "block";
+  activePage = id;
 }
 
-showPage("home");
+// start our code
+
+showPage(activePage);
